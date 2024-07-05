7179797f273b3678c6eca57a2eeec21f801bf1f9
Felipe Sereno Soso <182220040@senacinformatica.br>
2024-07-04 21:32:08 -0300

    metodologia

--- connection.php
+++ connection.php
@@ -13,16 +13,14 @@ if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
-// Dados a serem inseridos
-
-$nome = "rudi";
-$profissao = "dev";
-$telefone = "0000000";
-$email = "rudi@example.com";
-$setor = "publico";
-
-// Preparando a consulta SQL
-$sqli = "INSERT INTO user (nome, profissao, telefone, email, setor) VALUES ('$nome', '$profissao', '$telefone', '$email', '$setor')";
+// Capturando os dados do formulário
+$nome = 'nome';
+$pofissao = 'profissao';
+$telefone = 'telefone';
+$email = 'email';
+$setor = 'modalidade';
+// Preparando a conulta SQL
+$sqli = "INSERT INTO usuario (nome, profissao, telefone, email, setor) VALUES ('$nome', '$profissao', '$telefone', '$email', '$setor')";
 
 // Executando a consulta
 if ($conn->query($sqli) === TRUE) {
@@ -30,7 +28,6 @@ if ($conn->query($sqli) === TRUE) {
 } else {
     echo "Erro: " . $sqli . "<br>" . $conn->error;
 }
-
 // Fechando a conexão
 $conn->close();
 ?>
