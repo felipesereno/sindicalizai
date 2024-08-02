1fa384924ab7448af5ef0f2c4cc967cf0a8744cc
Felipe Soso <felipeserenososo@gmail.com>
2024-08-01 21:14:21 -0300

    preparação para hospedagem

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sindicalizaí</title>
+    <!--STYLESHEET-->
+    <link rel="stylesheet" href="src/css/style.css">
+    <!--FAVICON-->
+    <link rel="icon" href="src/img/raise-hand.png">
+    <script src="src/js/feed.js" defer></script>
+</head>
+<body>
+    <header>
+        <h1>Sindicalizaí</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">INÍCIO</a></li>
+                <li><a href="doc.html">PORQUÊ SE SINDICALIZAR</a></li>
+                <li><a href="map.html">MAPA DE SINDICATOS</a></li>
+                <li><a href="form.php">ASSOCIE-SE AO MLC</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="content-container">
+        <div class="half-width">
+            <h2>Sindicalizaí é um projeto desenvolvido pelo Núcleo de Trabalhadores da Informação e Comunicação do Movimento Luta de Classes (MLC) do Rio Grande do Sul e tem o objetivo de ajudar trabalhadores a encontrarem seus sindicatos.</h2>
+        </div>
+        <div class="half-width">
+            <div id="feed" class="carousel"></div>
+        </div>
+    </div>
+    <footer>
+        <div class="social">
+            <a href="https://www.instagram.com/mlc.rs/"><img src="src/img/instagram.png"  alt="Instagram do MLC RS"></a>
+        </div>
+    </footer>
+</body>
+</html>
